986804261cf564d6471f4181a61a964ccafe4581
Mathew Robinson <mathew@chasinglogic.io>
2025-09-23 19:06:21 +0100

    add catppuccin-mocha theme for ghostty

--- hosts/common/users/chasinglogic.nix
+++ hosts/common/users/chasinglogic.nix
@@ -1,26 +1,26 @@
 {
-    pkgs,
-    config,
-    lib,
-    ...
+  pkgs,
+  config,
+  lib,
+  ...
 }: {
-    # TODO: set this up when I get sops in here
-    # users.mutableUsers = false;
+  # TODO: set this up when I get sops in here
+  # users.mutableUsers = false;
 
-    users.users.chasinglogic = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-            # TODO: read these from file
-        ];
-        extraGroups = [
-            "wheel"
-            "networkmanager"
-            "docker"
-            "libvirtd"
-            "wireshark"
-            "podman"
-            "git"
-        ];
-        packages = [pkgs.home-manager];
-    };
-} 
+  users.users.chasinglogic = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      # TODO: read these from file
+    ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "docker"
+      "libvirtd"
+      "wireshark"
+      "podman"
+      "git"
+    ];
+    packages = [pkgs.home-manager];
+  };
+}
