8d5e4b0026f2745cc47cb95747950eba6029bd2f
Mathew Robinson <mathew@chasinglogic.io>
2025-09-29 14:34:45 +0100

    setup raza's static ip address

--- hosts/raza/default.nix
+++ hosts/raza/default.nix
@@ -15,7 +15,24 @@
     };
   };
 
-  networking.hostName = "raza";
+  networking = {
+    hostName = "raza";
+
+    defaultGateway = "192.168.1.1";
+    nameservers = [
+      "1.1.1.1"
+      "1.1.1.2"
+    ];
+
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.1.2";
+          prefixLength = 24;
+        }
+      ];
+    };
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
