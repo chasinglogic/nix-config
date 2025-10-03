68b632b6bc54d3af774e37e7ba13dcbb08745ef0
Mathew Robinson <mathew@chasinglogic.io>
2025-10-03 23:56:07 +0100

    name the first window nvim

--- dotfiles/.config/fish/functions/t.fish
+++ dotfiles/.config/fish/functions/t.fish
@@ -3,7 +3,7 @@ function t
 
     if ! tmux has-session -t $session_name
         if test -d .git
-            tmux new-session -d -s $session_name 'nvim .'
+            tmux new-session -n 'nvim' -d -s $session_name 'nvim .'
             tmux new-window
             tmux select-window -t 1
         else
